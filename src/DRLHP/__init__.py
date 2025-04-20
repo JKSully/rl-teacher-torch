5bdce81e479e8eff0b86a95f0213b5dac7341db5
jksully <jksully@umich.edu>
2025-04-20 14:30:22 -0400

    changed rl library to stable baselines

--- src/DRLHP/__init__.py
+++
@@ -1,7 +0,0 @@
-from gymnasium.envs import register
-
-register(
-    id='Particle-v0',
-    entry_point='DRLHP.particle:Particle',
-    max_episode_steps=3,  # Each episode has exactly this many steps
-)
